5759d95099d0881d6bbbc1470ac94d212f96a55a
TurabiRahman <rahman.turabi.10@gmail.com>
2026-04-05 23:54:06 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const { title } = require('process');
+
+const app = express();
+
+//app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+let posts = [
+    {id : 1, title: "Post one"},
+    {id : 2, title: "Post two"},
+    {id : 3, title: "Post three"},
+]
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+})
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
