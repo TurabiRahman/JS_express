65d35d0645723e08b99431bffcd40fbd41133e4d
TurabiRahman <rahman.turabi.10@gmail.com>
2026-04-06 17:04:40 +0600

    query strings - 6-4-26

--- server.js
+++ server.js
@@ -23,15 +23,25 @@ let posts = [
     {id : 1, title: "Post one"},
     {id : 2, title: "Post two"},
     {id : 3, title: "Post three"},
-]
+];
 
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
-})
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0)
+    {
+        res.json(posts.slice(0, limit));
+    }
+    else
+    {
+        res.json(posts);
+    }
+});
 
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
     res.json(posts.filter((post) => post.id === id));
-})
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
