a85ac59df69003f3a7edb4adfc5c98dfe691f56b
TurabiRahman <rahman.turabi.10@gmail.com>
2026-04-22 16:32:40 +0600

    feat: express_function

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
 import 'dotenv/config';
 
 import express from 'express';
-// import path from 'path';
-// import posts from './routes/posts.js';
 
 
 const port = process.env.PORT || 8080;
 
 
 const app = express();
-app.use(express.json());
-
-
-//app.use(express.urlencoded({extended : false}));
-//app.use(express.static(path.join(__dirname, 'public')))
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// });
-
-
-//app.use('/api/posts', posts);
-
-
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     res.send('This is the first code with sumit');
