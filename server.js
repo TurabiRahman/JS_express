b61815b5418ac4f0245cab63c7cec8fa6a326e29
TurabiRahman <rahman.turabi.10@gmail.com>
2026-04-22 12:43:16 +0600

    feat: post_request

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 import 'dotenv/config';
 
 import express from 'express';
-import path from 'path';
-import posts from './routes/posts.js';
+// import path from 'path';
+// import posts from './routes/posts.js';
 
 
 const port = process.env.PORT || 8080;
 
 
 const app = express();
-
-//body paser  middleware
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
 
 
-app.use('/api/posts', posts);
-
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
+//app.use(express.urlencoded({extended : false}));
 //app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/', (req, res) => {
@@ -29,3 +22,20 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 // app.get('/about', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 // });
+
+
+//app.use('/api/posts', posts);
+
+
+
+app.get('/', (req, res) => {
+    res.send('This is the first code with sumit');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Data received successfully');
+}); 
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
