009295409ee81f3a677b1fd79b39d38cf73b32a3
TurabiRahman <rahman.turabi.10@gmail.com>
2026-06-12 12:32:07 +0600

    js array lec 21

--- public/test.js
+++ public/test.js
@@ -36,3 +36,14 @@ function printFruits(value)
 }   
 
 fruits.forEach(printFruits);
+
+const cars = [
+  {type:"Volvo", year:2016},
+  {type:"Saab", year:2001},
+  {type:"BMW", year:2010}
+];
+
+//cars.sort(function(a, b){return a.year - b.year});
+cars.sort();
+
+console.log(cars);
