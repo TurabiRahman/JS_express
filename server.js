d379cd0ed9c6c8e56b855559a3c1122c5da375b2
TurabiRahman <rahman.turabi.10@gmail.com>
2026-06-22 21:12:53 +0600

    js- express start

--- server.js
+++ server.js
@@ -14,81 +14,81 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    console.log(typeof req.body);
     res.send('Data received successfully');
 }); 
 
-app.param('id', (req, res, next, id) => {
-    const user = {
-        userId: id,
-        name: `User ${id + 1}`,
-    };
-    req.user = user;
-    next();     
-    });
+// app.param('id', (req, res, next, id) => {
+//     const user = {
+//         userId: id,
+//         name: `User ${id + 1}`,
+//     };
+//     req.user = user;
+//     next();     
+//     });
 
-app.get('/user/:id', (req, res) => {
-    const userId = req.params.id;
-    console.log(req.user);          
-    res.send(`Welcome to app, user ${userId}!`);
-});
+// app.get('/user/:id', (req, res) => {
+//     const userId = req.params.id;
+//     console.log(req.user);          
+//     res.send(`Welcome to app, user ${userId}!`);
+// });
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.get('/about', (req, res) => {
-    res.render('page/about', {
-        name : 'Turabi_Rahman',
-    });
+// app.get('/about', (req, res) => {
+//     res.render('page/about', {
+//         name : 'Turabi_Rahman',
+//     });
 
-    //res.sendStatus(200);
-});
+//     //res.sendStatus(200);
+// });
 
-app.get('/contact', (req, res) => {
-    res.format({
-        'text/plain': () => {
-            res.send('HI');
-        },
-        'text/html': () => {
-            res.render('page/about', {
-                name : 'Mizanur Rahman',
-            });
-        },
-        'application/json': () => {
-            res.json({ message: 'You are not around the world' });
-        },
-        default: () => {
-            res.status(406).send('Not Acceptable');
-        }
-    });
-});
+// app.get('/contact', (req, res) => {
+//     res.format({
+//         'text/plain': () => {
+//             res.send('HI');
+//         },
+//         'text/html': () => {
+//             res.render('page/about', {
+//                 name : 'Mizanur Rahman',
+//             });
+//         },
+//         'application/json': () => {
+//             res.json({ message: 'You are not around the world' });
+//         },
+//         default: () => {
+//             res.status(406).send('Not Acceptable');
+//         }
+//     });
+// });
 
-const myMiddleware = (req, res, next) => {
-    console.log('This is my custom middleware');
-    next();
-};
-
-
-
-app.use(myMiddleware);
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+// const myMiddleware = (req, res, next) => {
+//     console.log('This is my custom middleware');
+//     next();
+// };
 
 
 
+// app.use(myMiddleware);
 
-
-// function logRequest(req, res, next) {
+// const logger = (req, res, next) => {
 //     console.log(`${req.method} ${req.url}`);
 //     next();
-// }
+// };
 
-//app.use(logRequest);
 
-app.get('/middleware', (req, res) => {
-    res.send('This route uses custom middleware');
-});
+
+
+
+// // function logRequest(req, res, next) {
+// //     console.log(`${req.method} ${req.url}`);
+// //     next();
+// // }
+
+// //app.use(logRequest);
+
+// app.get('/middleware', (req, res) => {
+//     res.send('This route uses custom middleware');
+// });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
