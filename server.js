7a4868050a626b7dec08efdcacb9a73be38f2fab
TurabiRahman <rahman.turabi.10@gmail.com>
2026-06-24 02:54:17 +0600

    js-express methods

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 const port = process.env.PORT || 8080;
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.json());
 
 app.use(express.static('public/'));
@@ -15,37 +16,41 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(typeof req.body);
+    console.log(req.body);
     res.send('Data received successfully');
 }); 
 
+app.param('id', (req, res, next, id) => {
+    const user = {
+        userId: Number(id),
+        name: `User ${Number(id) + 1}`,
+    };
+    req.user = user;
+    next();     
+    });
+
+app.get('/user/:id', (req, res) => {
+    //console.log(typeof req.params.id);
+    const userId = req.params.id;
+    console.log(req.user);          
+    res.send(`Welcome to app, user ${userId}!`);
+});
+
+app.get('/about', (req, res) => {
+    res.render('page/about', {
+        name : 'Turabi_Rahman',
+    });
+
+    //res.sendStatus(200);
+});
 
 app.listen(port, () => console.log(`✅ Server is running on port ${port}`));
 
-// app.param('id', (req, res, next, id) => {
-//     const user = {
-//         userId: id,
-//         name: `User ${id + 1}`,
-//     };
-//     req.user = user;
-//     next();     
-//     });
 
-// app.get('/user/:id', (req, res) => {
-//     const userId = req.params.id;
-//     console.log(req.user);          
-//     res.send(`Welcome to app, user ${userId}!`);
-// });
 
 // app.set('view engine', 'ejs');
 
-// app.get('/about', (req, res) => {
-//     res.render('page/about', {
-//         name : 'Turabi_Rahman',
-//     });
 
-//     //res.sendStatus(200);
-// });
 
 // app.get('/contact', (req, res) => {
 //     res.format({
