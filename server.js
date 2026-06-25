5a308c2c3a96ef884cf09885414e9bf6f2a4fa36
TurabiRahman <rahman.turabi.10@gmail.com>
2026-06-25 13:30:45 +0600

    js-express response

--- server.js
+++ server.js
@@ -37,16 +37,38 @@ app.get('/user/:id', (req, res) => {
     const userId = req.params.id;
     //console.log(req.user);
     console.log(req.cookies);          
-    res.send(`Welcome to app, user ${userId}!`);
+    //res.send(`Welcome to app, user ${userId}!`);
+    res.status(200).send(`Welcome to app, user ${userId}!`);
 });
 
 app.get('/about', (req, res) => {
     res.render('page/about', {
         name : 'Turabi_Rahman',
     });
-
-    //res.sendStatus(200);
 });
+    //res.sendStatus(200);
+
+
+    app.get('/contact', (req, res) => {
+    res.format({
+        'text/plain': () => {
+            res.send('HI');
+        },
+        'text/html': () => {
+            res.render('page/about', {
+                name : 'Mizanur Rahman',
+            });
+        },
+        'application/json': () => {
+            res.json({ message: 'You are not around the world' });
+        },
+        default: () => {
+            res.status(406).send('Not Acceptable');
+        }
+    });
+});
+
+
 
 app.listen(port, () => console.log(`✅ Server is running on port ${port}`));
 
@@ -56,24 +78,7 @@ app.listen(port, () => console.log(`✅ Server is running on port ${port}`));
 
 
 
-// app.get('/contact', (req, res) => {
-//     res.format({
-//         'text/plain': () => {
-//             res.send('HI');
-//         },
-//         'text/html': () => {
-//             res.render('page/about', {
-//                 name : 'Mizanur Rahman',
-//             });
-//         },
-//         'application/json': () => {
-//             res.json({ message: 'You are not around the world' });
-//         },
-//         default: () => {
-//             res.status(406).send('Not Acceptable');
-//         }
-//     });
-// });
+
 
 // const myMiddleware = (req, res, next) => {
 //     console.log('This is my custom middleware');
